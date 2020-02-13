97bbabc355c22a0c3cc3e1ce514d9cd9914adb6f
Marco <costa.1691388@studenti.uniroma1.it>
2020-02-13 11:13:15 +0100

    Started android app development

---
+++ Android/WeatherApp/app/src/main/java/uni/mobile/weatherapp/ResetPasswordActivity.java
@@ -0,0 +1,62 @@
+package uni.mobile.weatherapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    private EditText inputEmail;
+    private Button btnReset;
+    private FirebaseAuth auth;
+
+    @SuppressLint("WrongViewCast")
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+        inputEmail = (EditText) findViewById(R.id.EditTextSurname);
+        btnReset = (Button) findViewById(R.id.btn_reset_password);
+        auth = FirebaseAuth.getInstance();
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString().trim();
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplication(), "Enter your mail address", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(ResetPasswordActivity.this, "We send you an e-mail", Toast.LENGTH_SHORT).show();
+
+                                } else {
+                                    Toast.makeText(ResetPasswordActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+    }
+
+    public void NavigateSignUp(View v) {
+        Intent inent = new Intent(this, SignUpActivity.class);
+        startActivity(inent);
+    }
+}
---
+++ Android/WeatherApp/app/src/main/java/uni/mobile/weatherapp/Userinformation.java
@@ -0,0 +1,25 @@
+package uni.mobile.weatherapp;
+
+class Userinformation {
+    public String name;
+    public String surname;
+    public String phoneno;
+
+    public Userinformation(){
+    }
+
+    public Userinformation(String name,String surname, String phoneno){
+        this.name = name;
+        this.surname = surname;
+        this.phoneno = phoneno;
+    }
+    public String getUserName() {
+        return name;
+    }
+    public String getUserSurname() {
+        return surname;
+    }
+    public String getUserPhoneno() {
+        return phoneno;
+    }
+}
