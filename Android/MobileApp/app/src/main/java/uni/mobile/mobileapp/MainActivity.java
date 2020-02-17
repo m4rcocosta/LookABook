ce11136268925dc0fd6368ecac8e281308c7e9e8
Marco <costa.1691388@studenti.uniroma1.it>
2020-02-18 00:55:07 +0100

    Added theme switcher

--- Android/MobileApp/app/src/main/java/uni/mobile/mobileapp/MainActivity.java
+++ Android/MobileApp/app/src/main/java/uni/mobile/mobileapp/MainActivity.java
@@ -2,19 +2,31 @@ package uni.mobile.mobileapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SharedPreferences preferenceManager;
+    private int theme;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        preferenceManager = PreferenceManager.getDefaultSharedPreferences(this);
+        theme = preferenceManager.getInt("Theme", AppCompatDelegate.MODE_NIGHT_NO);
+        AppCompatDelegate.setDefaultNightMode(theme);
+
         setContentView(R.layout.activity_main);
+
         //Check current user
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         firebaseAuth.addAuthStateListener(authStateListener);
--- Android/MobileApp/app/src/main/java/uni/mobile/mobileapp/SettingsActivity.java
+++ Android/MobileApp/app/src/main/java/uni/mobile/mobileapp/SettingsActivity.java
@@ -2,20 +2,44 @@ package uni.mobile.mobileapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Button Variable
+    private Button buttonDayMode;
+    private Button buttonNightMode;
+    private Button buttonAutoMode;
+    private Button buttonBatteryMode;
+
+    private SharedPreferences preferenceManager;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        initViews();
+        initListeners();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) buttonBatteryMode.setVisibility(View.GONE);
+        else buttonAutoMode.setVisibility(View.GONE);
+
+        preferenceManager = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = preferenceManager.edit();
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
 
@@ -39,5 +63,58 @@ public class SettingsActivity extends AppCompatActivity {
                         return true;
                     }
                 });
+    }
+
+    /**
+     * method to initialize the views
+     */
+    private void initViews() {
+        buttonDayMode = (Button) findViewById(R.id.buttonDayMode);
+        buttonNightMode = (Button) findViewById(R.id.buttonNightMode);
+        buttonAutoMode = (Button) findViewById(R.id.buttonAutoMode);
+        buttonBatteryMode = (Button) findViewById(R.id.buttonBatteryMode);
+
+    }
+
+    /**
+     * method to initialize the listeners
+     */
+    private void initListeners() {
+        buttonAutoMode.setOnClickListener(this);
+        buttonDayMode.setOnClickListener(this);
+        buttonNightMode.setOnClickListener(this);
+    }
+
+    /**
+     * onClick Listener to capture button click
+     *
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.buttonDayMode:
+                editor.putInt("Theme", AppCompatDelegate.MODE_NIGHT_NO);
+                editor.commit();
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                break;
+            case R.id.buttonNightMode:
+                editor.putInt("Theme", AppCompatDelegate.MODE_NIGHT_YES);
+                editor.commit();
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                break;
+            case R.id.buttonAutoMode:
+                editor.putInt("Theme", AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                editor.commit();
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                break;
+            case R.id.buttonBatteryMode:
+                editor.putInt("Theme", AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
+                editor.commit();
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
+                break;
+        }
+
     }
 }
