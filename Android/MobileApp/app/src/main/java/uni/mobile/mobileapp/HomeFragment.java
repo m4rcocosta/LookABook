572d5c4c4058dca9d3a6e8c5975a7474d9c50eb6
Marco <costa.1691388@studenti.uniroma1.it>
2020-02-19 02:59:49 +0100

    Added DIAG rooms json for trying :)

--- Android/MobileApp/app/src/main/java/uni/mobile/mobileapp/HomeFragment.java
+++ Android/MobileApp/app/src/main/java/uni/mobile/mobileapp/HomeFragment.java
@@ -1,22 +1,131 @@
 package uni.mobile.mobileapp;
 
-import android.content.Context;
-import android.net.Uri;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
 
 public class HomeFragment extends Fragment {
+    private TextView tw;
+
+    private String url = "https://www.diag.uniroma1.it/pannello/?q=export_json";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        tw = view.findViewById(R.id.hometw);
+        tw.setMovementMethod(new ScrollingMovementMethod());
+
+        // Initialize a new RequestQueue instance
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+
+        // Initialize a new JsonArrayRequest instance
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,  url, null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // Do something with response
+                        //mTextView.setText(response.toString());
+
+                        // Process the JSON
+                        ArrayList<JSONObject> rooms = new ArrayList<JSONObject>();
+                        // Loop through the array elements
+                        for(int i = 0; i < response.length(); i++) {
+                            // Get current json object
+                            try {
+                                rooms.add(response.getJSONObject(i));
+                            } catch (JSONException e) {
+                                // TODO Auto-generated catch block
+                                e.printStackTrace();
+                            }
+                        }
+
+                        Collections.sort(rooms, new Comparator<JSONObject>() {
+
+                            @Override
+                            public int compare(JSONObject lhs, JSONObject rhs) {
+                                // TODO Auto-generated method stub
+
+                                try {
+                                    return (lhs.getString("nome_aula").toLowerCase().compareTo(rhs.getString("nome_aula").toLowerCase()));
+                                } catch (JSONException e) {
+                                    // TODO Auto-generated catch block
+                                    e.printStackTrace();
+                                    return 0;
+                                }
+                            }
+                        });
+
+                        Iterator it = rooms.iterator();
+                        while (it.hasNext()) {
+                            JSONObject room = (JSONObject) it.next();
+
+                            try {
+                                String name = room.getString("nome_aula");
+                                String description = room.getString("Descrizione");
+                                String start = room.getString("ora_inizio") + room.getString("minuti_inizio");
+                                String end = room.getString("ora_fine") + room.getString("minuti_fine");
+                                // Display the formatted json data in text view
+                                tw.append("Aula: " + name +"\nDescription: " + description + "\nTime: " + start + "-" + end);
+                                tw.append("\n\n");
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+
+/*
+                                // Get the current student (json object) data
+                                String name = rooms.getString("nome_aula");
+                                String description = rooms.getString("Descrizione");
+                                String start = rooms.getString("ora_inizio") + ":" + rooms.getString("minuti_inizio");
+                                String end = rooms.getString("ora_fine") + ":" + rooms.getString("minuti_fine");
+
+                                // Display the formatted json data in text view
+                                tw.append("Aula: " + name +"\nDescription: " + description + "\nTime: " + start + "-" + end);
+                                tw.append("\n\n");*/
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        // Do something when error occurred
+                        //Snackbar.make(mCLayout, "Error...", Snackbar.LENGTH_LONG).show();
+                    }
+                }
+        );
+
+        // Add JsonArrayRequest to the RequestQueue
+        requestQueue.add(jsonArrayRequest);
     }
 }
--- Android/MobileApp/app/src/main/java/uni/mobile/mobileapp/SignInActivity.java
+++ Android/MobileApp/app/src/main/java/uni/mobile/mobileapp/SignInActivity.java
@@ -6,22 +6,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class SignInActivity extends AppCompatActivity {
 
     private EditText SignInMail, SignInPass;
     private FirebaseAuth auth;
     private MaterialButton SignInButton;
+    private com.google.android.gms.common.SignInButton GoogleSignInButton;
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +48,7 @@ public class SignInActivity extends AppCompatActivity {
         SignInMail = findViewById(R.id.SignInMail);
         SignInPass = findViewById(R.id.SignInPass);
         SignInButton = findViewById(R.id.SignInButton);
+        GoogleSignInButton = findViewById(R.id.GoogleSignInButton);
 
         //Get Firebase auth instance
         auth = FirebaseAuth.getInstance();
@@ -44,6 +58,7 @@ public class SignInActivity extends AppCompatActivity {
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         SignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,6 +94,52 @@ public class SignInActivity extends AppCompatActivity {
                         });
             }
         });
+        GoogleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleSignIn();
+            }
+        });
+    }
+
+    private void googleSignIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = auth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+            }
+        }
     }
 
     public void NavigateSignUp(View v) {
