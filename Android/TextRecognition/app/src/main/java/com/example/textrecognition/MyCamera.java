a4d5beb233b57a9407ed076dc703b718b1cf7b73
not-a-genius <romagiuppo@gmail.com>
2020-02-20 12:15:09 +0100

    removed useless photo taker part

--- Android/TextRecognition/app/src/main/java/com/example/textrecognition/MyCamera.java
+++ Android/TextRecognition/app/src/main/java/com/example/textrecognition/MyCamera.java
@@ -3,14 +3,9 @@ package com.example.textrecognition;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-
-import android.net.Uri;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
@@ -18,7 +13,6 @@ import android.view.SurfaceView;
 import android.widget.TextView;
 
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.FileProvider;
 
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
@@ -27,10 +21,7 @@ import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class MyCamera {
 
@@ -118,55 +109,6 @@ public class MyCamera {
                  * Detect all the text from camera using TextBlock and the values into a stringBuilder
                  * which will then be set to the textView.
                  * */
-
-                String currentPhotoPath;
-
-
-                private File createImageFile() throws IOException {
-                    // Create an image file name
-                    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-                    String imageFileName = "JPEG_" + timeStamp + "_";
-                    File storageDir = ctx.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-                    File image = File.createTempFile(
-                            imageFileName,  /* prefix */
-                            ".jpg",         /* suffix */
-                            storageDir      /* directory */
-                    );
-
-                    // Save a file: path for use with ACTION_VIEW intents
-                    currentPhotoPath = image.getAbsolutePath();
-                    return image;
-                }
-
-                static final int REQUEST_TAKE_PHOTO = 1;
-
-                private void dispatchTakePictureIntent() {
-                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    // Ensure that there's a camera activity to handle the intent
-                    if (takePictureIntent.resolveActivity(ctx.getPackageManager()) != null) {
-                        // Create the File where the photo should go
-                        File photoFile = null;
-                        try {
-                            photoFile = createImageFile();
-                        } catch (IOException ex) {
-                            // Error occurred while creating the File
-
-                        }
-                        // Continue only if the File was successfully created
-                        if (photoFile != null) {
-                            Uri photoURI = FileProvider.getUriForFile(ctx,
-                                    "com.example.android.fileprovider",
-                                    photoFile);
-                            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                            //ctx.startActivityForResult();
-                        }
-                    }
-                }
-
-
-
-
-
 
 
                 @Override
