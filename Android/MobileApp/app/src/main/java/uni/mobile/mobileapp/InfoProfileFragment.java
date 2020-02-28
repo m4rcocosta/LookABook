e8e37b3d44c1f4dfbf1b57efeca7435eb3fd333b
Marco <costa.1691388@studenti.uniroma1.it>
2020-02-28 21:23:21 +0100

    Added material style

--- Android/MobileApp/app/src/main/java/uni/mobile/mobileapp/InfoProfileFragment.java
+++ Android/MobileApp/app/src/main/java/uni/mobile/mobileapp/InfoProfileFragment.java
@@ -4,7 +4,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
