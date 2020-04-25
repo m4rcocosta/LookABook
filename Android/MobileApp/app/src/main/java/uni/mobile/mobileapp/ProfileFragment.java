a7b77e6e69b9c151ff57fad8f5ef65d6b3f012b5
Marco <costa.1691388@studenti.uniroma1.it>
2020-04-25 03:15:10 +0200

    Added possibility to change profile pic for email users

--- Android/MobileApp/app/src/main/java/uni/mobile/mobileapp/ProfileFragment.java
+++ Android/MobileApp/app/src/main/java/uni/mobile/mobileapp/ProfileFragment.java
@@ -1,16 +1,22 @@
 package uni.mobile.mobileapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FacebookAuthProvider;
@@ -20,11 +26,15 @@ import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.auth.UserInfo;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.util.List;
 
 import uni.mobile.mobileapp.guiAdapters.TabAdapter;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class ProfileFragment extends Fragment {
@@ -33,7 +43,10 @@ public class ProfileFragment extends Fragment {
     private ViewPager viewPager;
     private ImageView profilePicImageView;
     private FirebaseAuth firebaseAuth;
+    private FirebaseUser user;
     private StorageReference storageReference;
+    private Uri imagePath;
+    private static int PICK_IMAGE = 123;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -67,7 +80,18 @@ public class ProfileFragment extends Fragment {
 
         profilePicImageView = view.findViewById(R.id.imageProfileImageView);
         firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
         storageReference = FirebaseStorage.getInstance().getReference();
+
+        if (getUserProvider(user).equals("FIREBASE")) profilePicImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent profileIntent = new Intent();
+                profileIntent.setType("image/*");
+                profileIntent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(profileIntent, "Select Image."), PICK_IMAGE);
+            }
+        });
 
         final FirebaseUser user = firebaseAuth.getCurrentUser();
 
@@ -88,6 +112,38 @@ public class ProfileFragment extends Fragment {
                 }
             });
         }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data.getData() != null) {
+            imagePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getApplicationContext().getContentResolver(), imagePath);
+                profilePicImageView.setImageBitmap(bitmap);
+                if (imagePath != null) sendUserImage();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void sendUserImage() {
+        // Get "User UID" from Firebase > Authentification > Users.
+        StorageReference imageReference = storageReference.child(firebaseAuth.getUid()).child("Images").child("Profile Pic"); //User id/Images/Profile Pic.jpg
+        UploadTask uploadTask = imageReference.putFile(imagePath);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getContext(), "Error: Uploading profile picture", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(getContext(), "Profile picture uploaded", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private String getUserProvider(FirebaseUser user) {
