18318064f37c8c7195c7a1311fa970c184614197
giuppo <giuppo@foo.com>
2020-04-27 00:55:04 +0200

    heroku ****

--- Backend/firebase-rails-modified/app/controllers/api_controller.rb
+++ Backend/firebase-rails-modified/app/controllers/api_controller.rb
@@ -5,7 +5,7 @@ class ApiController < ApplicationController
     before_action :authenticate_user
     
     wrap_parameters false 
-    skip_before_action :verify_authenticity_token
+    #skip_before_action :verify_authenticity_token
     
     
     
