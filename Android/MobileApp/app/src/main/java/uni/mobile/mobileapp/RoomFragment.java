237e720373051419fcfdd6939c587acd5914b82a
Marco <costa.1691388@studenti.uniroma1.it>
2020-04-28 01:43:11 +0200

    Started spinner selection

--- Android/MobileApp/app/src/main/java/uni/mobile/mobileapp/RoomFragment.java
+++ Android/MobileApp/app/src/main/java/uni/mobile/mobileapp/RoomFragment.java
@@ -1,6 +1,7 @@
 package uni.mobile.mobileapp;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,7 +11,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,6 +23,9 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.UserProfileChangeRequest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import uni.mobile.mobileapp.rest.House;
 import uni.mobile.mobileapp.rest.RestLocalMethods;
@@ -27,6 +35,7 @@ import uni.mobile.mobileapp.rest.Room;
 public class RoomFragment extends Fragment {
 
     private FloatingActionButton addRoomButton;
+    private Spinner userHouses;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -43,6 +52,57 @@ public class RoomFragment extends Fragment {
                 LayoutInflater inflater = getLayoutInflater();
                 View alertLayout = inflater.inflate(R.layout.layout_custom_dialog_add_room, null);
                 final EditText roomNameEditText = alertLayout.findViewById(R.id.roomName);
+                userHouses = alertLayout.findViewById(R.id.userHouses);
+                // Custom choices
+                List<String> choices = new ArrayList<>();
+                choices.add("Select an house!");
+                choices.add("A");
+                choices.add("B");
+                choices.add("C");
+                choices.add("D");
+                choices.add("E");
+
+                // Create an ArrayAdapter with custom choices
+                final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), R.layout.item_spinner, choices){
+                    @Override
+                    public boolean isEnabled(int position){
+                        if(position == 0) {
+                            return false;
+                        }
+                        else {
+                            return true;
+                        }
+                    }
+                    @Override
+                    public View getDropDownView(int position, View convertView,
+                                                ViewGroup parent) {
+                        View view = super.getDropDownView(position, convertView, parent);
+                        TextView tv = (TextView) view;
+                        if(position == 0) {
+                            // Set the hint text color gray
+                            tv.setTextColor(Color.GRAY);
+                        }
+                        return view;
+                    }
+                };
+
+                // Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(R.layout.item_spinner);
+
+                // Set the adapter to th spinner
+                userHouses.setAdapter(adapter);
+
+                userHouses.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        Toast.makeText(getContext(), userHouses.getSelectedItem() + " selected", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
                 new MaterialAlertDialogBuilder(getContext())
                         .setTitle("Create new room")
                         .setMessage("Insert the room name")
